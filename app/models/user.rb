73a7647f7223590cee631a59c331a7c45af991ca
daisuke-soda <68390854+daisuke-soda@users.noreply.github.com>
2020-09-04 03:32:30 +0900

    ミス

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,11 +8,29 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :following_user, through: :follower, source: :followed
+  has_many :follower_user, through: :followed, source: :follower
+
   validates :name, presence: true, length: {maximum: 20, minimum: 2}
   validates :introduction, length: {maximum: 50}
   
   def already_favorited?(book)
-    self.favorites.exists?(book_id: book.id)
-    
+    self.favorites.exists?(book_id: book.id)    
+  end
+
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+  
+  # ユーザーのフォローを外す
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+  
+  # フォロー確認をおこなう
+  def following?(user)
+    following_user.include?(user)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'home/about' => 'home#about'
   root to: "home#index"
   resources :users
+  resources :relationships, only: [:create, :destroy]
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
