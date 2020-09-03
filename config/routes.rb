b74defedd3eb7f5aab5359d14dc3744df8b78ebd
daisuke-soda <68390854+daisuke-soda@users.noreply.github.com>
2020-09-04 04:49:55 +0900

    ミス２

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
+
   devise_for :users
   get 'home/about' => 'home#about'
   root to: "home#index"
-  resources :users
+  resources :users do
+    resource :relationships, only: [:create, :destroy]
+    get :follows, on: :member
+    get :followers, on: :member
+  end
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
