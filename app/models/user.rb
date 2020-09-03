ff0cc12595af13cca4b54fa3ffedb801d0fe6adb
daisuke-soda <68390854+daisuke-soda@users.noreply.github.com>
2020-09-04 04:50:15 +0900

    Revert "ミス２"
    This reverts commit b74defedd3eb7f5aab5359d14dc3744df8b78ebd.

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,22 +8,11 @@ class User < ApplicationRecord
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
-  has_many :active_relationships, class_name: "Relationship", foreign_key: :following_id
-  has_many :followings, through: :active_relationships, source: :follower
-  has_many :passive_relationships, class_name: "Relationship", foreign_key: :follower_id
-  has_many :followers, through: :passive_relationships, source: :following
-
-
   validates :name, presence: true, length: {maximum: 20, minimum: 2}
   validates :introduction, length: {maximum: 50}
   
   def already_favorited?(book)
     self.favorites.exists?(book_id: book.id)
     
-  end
-
-  def followed_by?(user)
-    # 今自分(引数のuser)がフォローしようとしているユーザー(レシーバー)がフォローされているユーザー(つまりpassive)の中から、引数に渡されたユーザー(自分)がいるかどうかを調べる
-    passive_relationships.find_by(following_id: user.id).present?
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,8 @@
 Rails.application.routes.draw do
-
   devise_for :users
   get 'home/about' => 'home#about'
   root to: "home#index"
-  resources :users do
-    resource :relationships, only: [:create, :destroy]
-    get :follows, on: :member
-    get :followers, on: :member
-  end
+  resources :users
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
