3b4955b21974366958de3ae34badc49aeb5b1bb0
daisuke-soda <68390854+daisuke-soda@users.noreply.github.com>
2020-09-04 03:39:02 +0900

    Revert "ミス"
    This reverts commit 73a7647f7223590cee631a59c331a7c45af991ca.

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,6 @@ Rails.application.routes.draw do
   get 'home/about' => 'home#about'
   root to: "home#index"
   resources :users
-  resources :relationships, only: [:create, :destroy]
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
