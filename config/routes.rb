62c9da5f086b29ca9b8d2745183adb7e4d79b57b
daisuke-soda <68390854+daisuke-soda@users.noreply.github.com>
2020-09-04 17:27:03 +0900

    フォロー機能

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   get 'home/about' => 'home#about'
   root to: "home#index"
-  resources :users
+  resources :users,only: [:show,:index,:edit,:update,:create] do
+    member do
+      get :following, :followers
+    end
+  end
+  resources :relationships, only: [:create, :destroy]
   resources :books do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
